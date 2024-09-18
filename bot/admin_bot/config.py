bec95d582fd8b32814561527b9544020156989fa
Anatolii <abordovskyi@gmail.com>
2024-09-18 13:12:10 +0300

    Commit 2 добавляем admin

---
+++ bot/admin_bot/config.py
@@ -0,0 +1,18 @@
+# shared/config.py
+
+# Путь к базе данных
+DATABASE_PATH = r'C:\Users\USER\PycharmProjects\sandbox_project\sqlite.db'
+
+#DATABASE_PATH = r'C:\Users\USER\PycharmProjects\EventMaster\shared\db\sqlite.db'
+
+# Токен бота
+BOT_TOKEN = '7495955549:AAGG0PQNvFC-SN0PO4rx0WVi2HEeIM8mnVg'
+#7407529729:AAErOT5NBpMSO-V-HPAW-MDu_1WQt0TtXng - это токен picnic_bot
+
+# Идентификаторы чатов администратора и Ирины
+#ADMIN_CHAT_ID = 542067858
+# ADMIN_CHAT_ID = 921408292
+#IRA_CHAT_ID = 884649958
+
+
+
