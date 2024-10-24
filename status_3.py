c822a44fc0c212f5f740e642fba68dde7872cef8
Anatolii <abordovskyi@gmail.com>
2024-10-24 08:28:31 +0300

    Commit 51 корректировка коннекта для избежания зацикливания

--- status_3.py
+++ status_3.py
@@ -1,13 +1,33 @@
 import logging
+import os
+
 import psycopg2
+from psycopg2 import OperationalError
 
 from bot.picnic_bot.constants import ORDER_STATUS
-from bot.picnic_bot.main import create_connection
+#from bot.picnic_bot.main import create_connection
+
+
+def create_flask_to_db_connection():
+    """Создает соединение с базой данных PostgreSQL."""
+    try:
+        # Получаем параметры подключения из переменных окружения
+        conn = psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
+        )
+        logging.info("Database connected")
+        return conn
+    except psycopg2.OperationalError as e:
+        logging.error(f"Error connecting to database: {e}")
+        return None
 
 
 # Функция для обновления статуса заказа по order_id
 def update_order_status_to_paid(order_id):
-    conn = create_connection()  # Подключение к базе данных PostgreSQL
+    conn = create_flask_to_db_connection()  # Подключение к базе данных PostgreSQL
 
     if conn is not None:
         try:
